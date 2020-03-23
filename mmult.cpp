27d6a808daf72a1606ccdb3c8c883f70d0c7cbf4
anuj08 <anujsngh827@gmail.com>
2020-03-23 21:42:36 +0530

    Created Matrix Template

--- mmult.cpp
+++ mmult.cpp
@@ -1,32 +1,67 @@
 #include<fstream>
 #include<string>
+#include<sstream>
 
 
 template <class T>
 class Matrix {
     private:
         T *data;
-        int row, int col;
+        int rows, cols;
+
     public:
-        //filename, rows, columns
+        //filename, rows, columns, stored in row major format
         Matrix(std::string filename, int r, int c){
-            row = r;
-            col = c;
+            rows = r;
+            cols = c;
             data = new T[rows*cols];
             std::ifstream fin;
             fin.open(filename);
 
             std::string line;
-            int nums=0;
+            int i=0;
             if(fin.is_open()){
-                while(getline(fin, line) && nums <= rows*cols){
-                    
+                while(getline(fin, line) && i <= rows*cols){
+                    std::istringstream iss(line);
+                    while(i < rows*cols && iss >> data[i]){
+                        i++;
+                    }
                 }
             }
-            
+            fin.close();
         }
         //filename, assume first line contains rows columns
         Matrix(std::string filename){
+            std::ifstream fin;
+            fin.open(filename);
+            std::string line;
+            if(fin.is_open()){
+                getline(fin, line);
+                std::istringstream iss(line);
+                iss >> rows;
+                iss >> cols;
+                data = new T[rows*cols];
+                int i=0;
+                while(getline(fin, line) && i <= rows*cols){
+                    std::istringstream iss(line);
+                    while(i < rows*cols && iss >> data[i]){
+                        i++;
+                    }
+                }
+            }
+            fin.close();
+        }
 
+        T& at(int r, int c){
+            return data[r*rows + c];
+        }
+
+        void print(){
+            for(int i=0; i<rows; ++i){
+                for(int j=0; j<cols; ++j){
+                    std::cout<<data[i*rows + j]<<" ";
+                }
+                std::cout<<std::endl;
+            }
         }
 };
